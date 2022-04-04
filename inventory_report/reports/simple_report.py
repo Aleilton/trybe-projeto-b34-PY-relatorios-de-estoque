1dea52e9e98e2f0939a2f1f854ab171665ac2a69
Aleilton Chavenco <aleilton@gmail.com>
2022-04-04 07:46:34 -0300

    refactor: simpleReport refactor returns

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -6,36 +6,36 @@ class SimpleReport:
     @classmethod
     def generate(cls, lista: list) -> str:
         return (
+            "Data de fabricação mais antiga: "
             f"{cls.oldest_manufacturing_date(lista)}\n"
+            "Data de validade mais próxima: "
             f"{cls.closest_expiration_date(lista)}\n"
-            f"{cls.company_stocked_products(lista)}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            f"{sorted(cls.company_stocked_products(lista))[-1]}\n"
         )
 
-    @classmethod
-    def oldest_manufacturing_date(cls, lista: list) -> str:
+    @staticmethod
+    def oldest_manufacturing_date(lista: list) -> str:
         oldest_dates = set()
         for item in lista:
             oldest_dates.add(item["data_de_fabricacao"])
-        return (f"Data de fabricação mais antiga: {sorted(oldest_dates)[0]}")
+        return sorted(oldest_dates)[0]
 
-    @classmethod
-    def closest_expiration_date(cls, lista: list) -> str:
+    @staticmethod
+    def closest_expiration_date(lista: list) -> str:
         closest_dates = set()
         for item in lista:
             item_data = datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
             if item_data > datetime.now():
                 closest_dates.add(item["data_de_validade"])
-        return (f"Data de validade mais próxima: {sorted(closest_dates)[0]}")
+        return sorted(closest_dates)[0]
 
-    @classmethod
-    def company_stocked_products(cls, lista: list) -> str:
+    @staticmethod
+    def company_stocked_products(lista: list) -> dict:
         result = {}
         for item in lista:
             company = item["nome_da_empresa"]
             if company not in result:
                 result[company] = 0
             result[company] += 1
-        return (
-            "Empresa com maior quantidade de produtos estocados: "
-            f"{sorted(result)[-1]}"
-        )
+        return result

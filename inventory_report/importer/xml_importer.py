50d36f4b60ab5ac88e0a6322cd07edfa470e9d90
Aleilton Chavenco <aleilton@gmail.com>
2022-04-04 12:30:32 -0300

    feat: implement xml_importer class

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,15 @@
+import os
+import xmltodict
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+
+    def import_data(path) -> list[dict]:
+        # https://www.horadecodar.com.br/2021/04/17/extrair-extensao-do-arquivo-com-python/
+        file, ext = os.path.splitext(path)
+        if ext == ".xml":
+            with open(path) as file:
+                return xmltodict.parse(file.read())["dataset"]["record"]
+        else:
+            raise ValueError("Arquivo inválido")
